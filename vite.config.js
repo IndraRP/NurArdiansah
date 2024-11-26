6e70ee82f78e0cf4df07872989d5dc593c3ed8c6
IndraRP <indraridho08@gmail.com>
2024-11-26 20:19:05 +0700

    update pwa tapi belum bisa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/NurArdiansah"
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Nur Ardiansah',
+        short_name: 'Nur Ardiansah',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192x192.png', // Ganti ke PNG
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png', // Ganti ke PNG
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
